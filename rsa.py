87934f8555dca4a8fa8891b19b0984883c65acb8
M0rfes <42341113+M0rfes@users.noreply.github.com>
2019-04-20 16:16:13 +0530

    rsa

--- rsa.py
+++ rsa.py
@@ -1,21 +1,39 @@
-import Crypto
+from Crypto.Cipher import PKCS1_OAEP
 from Crypto.PublicKey import RSA
 from Crypto import Random
 import ast
 plainl_text = input("Enter your text \n")
-key_us = int(input("Enter an integer"))
-random_generator = Random.new().read
-key = RSA.generate(1024, random_generator)  # generate pub and priv key
-
-publickey = key.publickey()  # pub key export for exchange
-msg = [ord(char) for char in plainl_text]
-encrypted = [key.encrypt(pt, key_us) for pt in msg]
-# message to encrypt is in the above line 'encrypt this message'
-
-for ctext in encrypted:
-    print(ctext, '\n')  # ciphertext
+plainl_text = bytes(plainl_text, 'utf-8')
+num1 = int(input("Enter one Prime Number"))
+num2 = int(input("Enter another Prime Number"))
 
 
-decrypted = [chr(key.decrypt(ast.literal_eval(str(ct)))) for ct in encrypted]
+def gen_key(num1, num2):
+    random_generator = Random.new(num1, num2).read
+    key = RSA.generate(1024, random_generator)  # generate pub and privat key
+    f = open('mykey.pem', 'wb')
+    f.write(key.export_key('PEM'))
+    f.close()
+    return key
 
-print('decrypted:', "".join(decrypted))
+
+def encrypt(pText, num1, num2):
+    publickey = gen_key(num1, num2).publickey()  # pub key export for exchange
+    encryptor = PKCS1_OAEP.new(publickey)
+    cyText = encryptor.encrypt(plainl_text)
+    return cyText
+
+
+def decrypt(cyText):
+    f = open('mykey.pem', 'r')
+    key = RSA.import_key(f.read())
+    f.close()
+    decryptor = PKCS1_OAEP.new(key)
+    decrypted = decryptor.decrypt(ast.literal_eval(str(cyText)))
+    return decrypted
+
+
+cipher = encrypt(plainl_text, num1, num2)
+print("Encrypted text:\n", cipher)
+decrypted = decrypt(cipher)
+print("Decrypted Text:\t", decrypted)
